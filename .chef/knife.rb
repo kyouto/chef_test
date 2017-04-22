016d8eced0e15b06227252516f54c4b670aaa607
bamboo-yujiro <ch11.naji@gmail.com>
2017-04-22 15:03:07 +0900

    node gitignore 削除, knife.rb 修正

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,18 +9,23 @@ ssl_verify_mode  :verify_peer
 knife[:ssh_attribute] = "knife_zero.host"
 knife[:use_sudo] = true
 
-knife[:automatic_attribute_whitelist] = %w[
-  fqdn
-  os
-  os_version
-  hostname
-  ipaddress
-  roles
-  recipes
-  ipaddress
-  platform
-  platform_version
-  cloud
-  cloud_v2
-  chef_packages
+knife[:automatic_attribute_whitelist] = [
+  "fqdn/",
+  "os/",
+  "os_version/",
+  "hostname",
+  "ipaddress/",
+  "roles/",
+  "recipes/",
+  "ipaddress/",
+  "platform/",
+  "platform_version/",
+  "platform_version/",
+  "cloud/",
+  "cloud_v2/",
+  "ec2/ami_id/",
+  "ec2/instance_id/",
+  "ec2/instance_type/",
+  "ec2/placement_availability_zone/",
+  "chef_packages/"
 ]
